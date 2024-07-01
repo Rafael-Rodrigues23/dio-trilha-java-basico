5be27c16b1d21d0d9c73f1bf39da881e12840c92
Rafael-Rodrigues23 <rafaaelrodrigues@outlook.com>
2024-07-01 00:11:33 -0300

    teste finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -7,20 +7,21 @@ public class ContaTerminal {
 
         System.out.println("Por favor, digite o número Conta !");
         int numero = sc.nextInt();
-        System.out.println(numero);
+       
 
-        System.out.println("Por favor, digite o número Agência !");
-        String agencia = sc.nextLine();
-        System.out.println(agencia);
+        System.out.println("Por favor, digite a Agencia !");
+        String teste = sc.nextLine();
+        
+     
 
         System.out.println("Por favor, digite o seu Nome !");
         String nome = sc.nextLine();
-        System.out.println(nome);
+     
 
         System.out.println("Por favor, digite o seu saldo atual !");
         Double saldo = sc.nextDouble();
         System.out.println(saldo);
 
-        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");12
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + teste + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
